f8f96aa5499883358bb21983e35da6b6745eddb0
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-10-26 01:28:16 +0500

    Task5

---
+++ Tyuiu.ShaykhelislamovaDA.Sprint4.Task5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint4.Task5.V23.Lib;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint4.Task5.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] arr = new int[3, 3] { { -1, 2, -3 }, { -2, 2, 3 }, { 0, 2, 3 } };
+            int[,] wait = new int[3, 3] { { 0, 2, 0 }, { 0, 2, 3 }, { 0, 2, 3 } };
+            int[,] res = ds.Calculate(arr);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShaykhelislamovaDA.Sprint4.Task5.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint4.Task5.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+        int[,] arr = new int[5, 5];
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = rnd.Next(-4, 9);
+            }
+        }
+        Console.Title = "Спринт #4 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Двумерные массивы  (генератор случайных чисел)                          *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* случайными значениями в диапазоне от -4 до 8. Заменить отрицательные    *");
+        Console.WriteLine("* элементы на 0.                                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        for (int k = 0; k < arr.GetLength(0); k++)
+        {
+            for (int j = 0;j < arr.GetLength(1); j++)
+            {
+                Console.Write(ds.Calculate(arr)[k, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
